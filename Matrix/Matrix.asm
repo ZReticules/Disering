a967c5d3e97110263a8f646bcf210f1baf55c294
ZReticules <my_mailbox_228@mail.ru>
2023-08-05 12:58:00 +0300

    Matrix_Full

--- Matrix/Matrix.asm
+++ Matrix/Matrix.asm
@@ -6,6 +6,10 @@ include c:\libs\pstring\pstring.inc
 include c:\libs\heap\heap.inc
 include c:\libs\array\array.inc
 
+VERSION M520
+
+LOCALS __
+
 matrix_head struc
     db ?                    ;block type
     dw ?                    ;сегмент владельца
@@ -57,13 +61,77 @@ Matrix_GetColumnsCount proc C far uses ds
     ret
 endp
 
-Matrix_GetElement proc C far uses ds dx
+Matrix_GetElement proc C far uses ds cx
 @@Matrix    equ [esp+8]
-@@RangeNum  equ [esp+10]
-@@ColumnNum equ [esp+12]
+@@RangeNum  equ [esp+12]
+@@ColumnNum equ [esp+14]
+    xor eax, eax
     mov dx, @@RangeNum
-    call Matrix_GetRangeCount C, @@Matrix
-    
+    call Matrix_GetRangeCount C, dword ptr @@Matrix
+    mul dx
+    add ax, @@ColumnNum
+    mov dx, @@Matrix[2]
+    dec dx
+    mov ds, dx
+    mov cl, ds:[MatrixType]
+    inc dx
+    mov ds, dx
+    cmp cl, 2
+        je __dword
+        jnp __byte
+        jl __word
+        __qword:
+            mov edx, ds:[eax*QWORD+4]
+            mov eax, ds:[eax*QWORD]
+        jmp __return
+        __dword:
+            mov eax, ds:[eax*DWORD]
+        jmp __return
+        __word:
+            mov ax, ds:[eax*WORD]
+        jmp __return
+        __byte:
+            mov al, ds:[eax*BYTE]
+    __return:
+    ret
+endp
+
+Matrix_SetElement proc C far uses ds ecx
+@@Matrix    equ [esp+10]
+@@RangeNum  equ [esp+14]
+@@ColumnNum equ [esp+16]
+@@NewValue  equ [esp+18]
+    xor eax, eax
+    mov dx, @@RangeNum
+    call Matrix_GetRangeCount C, dword ptr @@Matrix
+    mul dx
+    add ax, @@ColumnNum
+    mov dx, @@Matrix[2]
+    dec dx
+    mov ds, dx
+    mov cl, ds:[MatrixType]
+    inc dx
+    mov ds, dx
+    cmp cl, 2
+    mov ecx, eax
+    mov eax, @@NewValue
+    mov edx, @@NewValue+4
+    je __dword
+    jnp __byte
+    jl __word
+        __qword:
+            mov ds:[ecx*QWORD+4], edx
+            mov ds:[ecx*QWORD], eax
+        jmp __return
+        __dword:
+            mov ds:[ecx*DWORD], eax
+        jmp __return
+        __word:
+            mov ds:[ecx*WORD], ax
+        jmp __return
+        __byte:
+            mov ds:[ecx*BYTE], al
+    __return:
     ret
 endp
 
@@ -77,16 +145,14 @@ main:
     mov ax, @data
     mov ds, ax
     call $ method Heap:Init C, END_PROG
-    call Matrix_New C, WORD, 4, 4
+    call Matrix_New C, BYTE, 4, 4
     mov Mat1, eax
-    xor eax, eax
-    call $ method Array:GetLength C, Mat1
+    les di, Mat1
+    call Matrix_SetElement C, Mat1, 1, 0, 15
+    call Matrix_GetElement C, Mat1, 1, 0
     call $ method PString:Int32Str C, ds offset PStr1, eax
     call $ method PString:Write C, ds offset PStr1
     call $ method PString:Write C, ds offset TAB
-    call Matrix_GetColumnsCount C, Mat1
-    call $ method PString:Int32Str C, ds offset PStr1, eax
-    call $ method PString:Write C, ds offset PStr1
     mov ah, 4ch
     int 21h
 .fardata END_PROG
